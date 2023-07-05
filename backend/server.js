153ac6dec0f60b8aecbc703ed275b703f08b40f2
Sayali Sankhe <86399171+sayalisankhe5@users.noreply.github.com>
2023-07-05 16:25:59 +0530

    Additional changes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,16 @@ const express = require("express")
 
 const app = express()
 
-app.listen(4000,()=>{
-    console.log("listening on port 4000")
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+app.get("/",(req,res)=>{
+    res.json({message:"Hello Hi"})
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on port ${process.env.PORT}`)
 })
